fbba12a548edccac97e695c2c47cc15f1909dd84
dafnemus <dafnemus@gmail.com>
2022-07-05 18:14:45 -0300

    Hello API: - Created typeDefs query of greetings - created resolvers to respond greetings queries - created graphQL schema - added graphqlHTTP

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
+import { IResolvers } from '@graphql-tools/utils';
+import { makeExecutableSchema } from 'graphql-tools';
+import { GraphQLSchema } from 'graphql';
+import { graphqlHTTP } from 'express-graphql';
 
 const app = express();
 
@@ -8,12 +12,37 @@ app.use(cors());
 
 app.use(compression());
 
-app.use('/', (re, res) => {
-  res.send('Bienvenido, esta API usa GraphQL');
+const typeDefs = `
+  type Query {
+    greetingBasic: String!
+    greetingPerson(name: String!): String!
+    greetingWelcomeAPI: String!
+  }
+`;
+
+const resolvers: IResolvers = {
+  Query: {
+    greetingBasic(): string {
+      return 'Hello World';
+    },
+    greetingPerson(__: void, { name }): string {
+      return `Hello ${name}`;
+    },
+    greetingWelcomeAPI(): string {
+      return 'Welcome to Hello(API GraphQL)';
+    },
+  },
+};
+
+const schema: GraphQLSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
 });
+
+app.use('/', graphqlHTTP({ schema, graphiql: true }));
 
 const PORT = 5300;
 
 app.listen({ port: PORT }, () =>
-  console.log(`Hola mundo API GraphQL http://localhost:${PORT}`)
+  console.log(`Url Hello API GraphQL http://localhost:${PORT}/graphql`)
 );
