e2a6c559c1d73b6009918271fc9efe2e8b6521ec
dafnemus <dafnemus@gmail.com>
2022-07-07 17:21:54 -0300

    Server - Installed apollo-server-express - Conected apolloServer with the schema - created httpServer

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
+// import { graphqlHTTP } from 'express-graphql';
 import compression from 'compression';
 import cors from 'cors';
 import schema from './schema';
+import { ApolloServer } from 'apollo-server-express'
+import { createServer } from 'http';
 
 const app = express();
 
@@ -10,10 +12,23 @@ app.use(cors());
 
 app.use(compression());
 
-app.use('/', graphqlHTTP({ schema, graphiql: true }));
+// app.use('/', graphqlHTTP({ schema, graphiql: true })); with graphql express
+let server = null;
+
+const startServer = async () => {
+  server = new ApolloServer({
+    schema,
+    introspection: true
+  })
+  await server.start()
+  server.applyMiddleware({ app });
+}
+
 
 const PORT = 5300;
+startServer();
+const httpServer = createServer(app)
 
-app.listen({ port: PORT }, () =>
+httpServer.listen({ port: PORT }, () =>
   console.log(`Url Hello API GraphQL http://localhost:${PORT}/graphql`)
 );
